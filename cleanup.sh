190f54ef4bfe8b54da1855a589845cc2e6531f69
Reilly Wood <reilly.wood@icloud.com>
2020-02-11 23:19:10 +0000

    cp unit file instead of symlink

--- cleanup.sh
+++ cleanup.sh
@@ -1,5 +1,6 @@
 #! /usr/bin/bash
 sudo systemctl stop hello-service
 sudo systemctl disable hello-service
+sudo rm /etc/systemd/system/hello-service.service
 sudo journalctl --rotate
 sudo journalctl --vacuum-time=1s
